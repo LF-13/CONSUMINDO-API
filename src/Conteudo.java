2351d66b162cd7942ef059bd33e35784492865dc
LF-13 <l.fernandes0308@gmail.com>
2023-01-27 15:11:56 -0300

    Consumindo uma API da agência espacial NASA

---
+++ src/Conteudo.java
@@ -0,0 +1,19 @@
+public class Conteudo {
+
+    private final String tilulo;
+    private final String urlImagem;
+
+    public Conteudo(String tilulo, String urlImagem) {
+        this.tilulo = tilulo;
+        this.urlImagem = urlImagem;
+    }
+
+    public String getTilulo() {
+        return tilulo;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,47 @@
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        // top 250 filmes https://imdb-api.com/en/API/Top250Movies/k_o0rb5eyp
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_o0rb5eyp";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoIMDB();
+
+
+//                NASA
+//        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+//        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+        var gerado = new GeradoraDeFigurinha();
+
+        for (int i = 0; i < 3; i++) {
+
+            Conteudo conteudo = conteudos.get(i);
+
+
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTilulo() + ".png";
+
+
+            gerado.cria(inputStream, nomeArquivo);
+
+            System.out.println(conteudo.getTilulo());
+            System.out.println();
+
+
+        }
+
+    }
+
+}
+
+
